028b6dc71ae1f5a3a79f8c648b6f9fdb88bdb926
aklima12 <128598790+aklima12@users.noreply.github.com>
2023-05-19 11:20:36 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,8 @@
+// document.getElementById('demo').innerHTML="md jahid hossain";
+// document.getElementsByClassName('test')[0].innerHTML="Md rabbi";
+// document.querySelector('.test').innerHTML="ame and tomra";
+// document.querySelector('#demo').innerHTML="Hello madaripur peoples";
+var x=50;
+var y=100;
+var total=x+y;
+document.write(total)
